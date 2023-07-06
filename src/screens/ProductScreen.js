a35309aadfb31c81544c89d1b3015b16babbb5b4
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-07-05 21:06:48 -0300

    Conectar Frontend com Backend

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
 import Rating from "../components/Rating";
+import axios from "axios";
 import products from "../products";
 
 const ProductScreen = ({ match }) => {
   const product_id = useParams();
-  const product = products.find((p) => p._id == product_id.id);
+  // const product = products.find((p) => p._id == product_id.id);
+
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const { data } = await axios.get(`/api/products/${product_id.id}`);
+      setProduct(data);
+    }
+
+    fetchProducts();
+  }, []);
 
   return (
     <div>

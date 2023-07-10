a6878dabc9caf1f471cfcca1b15f5dbf8a9b2fc3
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-07-10 13:42:01 -0300

    Add to Cart Functionality

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
-// import {
-//   Link,
-//   useSearchParams,
-//   useParams,
-//   useNavigate,
-// } from "react-router-dom";
+import {
+  Link,
+  useSearchParams,
+  useParams,
+  useNavigate,
+} from "react-router-dom";
 import {
   Row,
   Col,
@@ -14,24 +14,24 @@ import {
   Button,
   Card,
 } from "react-bootstrap";
-// import Message from "../components/Message";
-// import { addToCart, removeFromCart } from "../actions/cartActions";
-// import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
+import { useDispatch, useSelector } from "react-redux";
 
 function CartScreen() {
-  // const navigate = useNavigate();
-  // const { id } = useParams();
-  // const [searchParams, setSearchParams] = useSearchParams();
-  // const qty = Number(searchParams.get("qty"));
-  // const dispatch = useDispatch();
-  // const cart = useSelector((state) => state.cart);
-  // const { cartItems } = cart;
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const qty = Number(searchParams.get("qty"));
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
 
-  // useEffect(() => {
-  //   if (id) {
-  //     dispatch(addToCart(id, qty));
-  //   }
-  // }, [dispatch, id, qty]);
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty));
+    }
+  }, [dispatch, id, qty]);
 
   // const removeFromCartHandler = (id) => {
   //   dispatch(removeFromCart(id));
